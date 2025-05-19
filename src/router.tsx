6a1f10bf3330b9eb195241be9b29c90f760c4c55
tensei <908739+tnsicdr@users.noreply.github.com>
2025-05-19 14:27:45 -0400

    fix: update base

--- src/router.tsx
+++ src/router.tsx
@@ -3,9 +3,11 @@ import { Home } from './screens/Home/Home';
 import { Root } from './screens/Root/Root';
 import { Project } from './screens/Project/Project';
 
+const BASE = import.meta.env.BASE_URL;
+
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: BASE,
     Component: Root,
     children: [
       { index: true, Component: Home },
